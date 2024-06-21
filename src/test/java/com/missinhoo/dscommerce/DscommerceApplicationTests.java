dd42fc82441e007b7548f31d99840720e2f532b0
Emerson <lyncoln_erc@hotmail.com>
2024-06-21 18:46:36 -0300

    first commit

---
+++ src/test/java/com/missinhoo/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missinhoo.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
